b0dd6651833e040a34cee66fa6ba5e3f4b018d0a
kazut <kazut@LAPTOP-S99BIQRN>
2018-06-17 20:17:29 +0900

    初回

---
+++ src/hashmap/Main.java
@@ -0,0 +1,40 @@
+package hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Map<String, Object> memberDB = new XMLMap();
+		Map<String, Object> kazuya = new XMLMap();
+		kazuya.put("ID", "1");
+		kazuya.put("name", "kazuya");
+		kazuya.put("hobby", "soccer");
+		memberDB.put(kazuya.get("ID").toString(), kazuya);
+		Map<String, Object> nakayama = new XMLMap();
+		nakayama.put("ID", "2");
+		nakayama.put("name", "nakayama");
+		nakayama.put("hobby", "baseball");
+		memberDB.put(nakayama.get("ID").toString(), nakayama);
+		Map<String, Object> tetsuya = new XMLMap();
+		tetsuya.put("ID", "3");
+		tetsuya.put("name", "tetsuya");
+		tetsuya.put("hobby", "mahjong");
+		memberDB.put(tetsuya.get("ID").toString(), tetsuya);
+		System.out.println(memberDB);
+	}
+
+}
+
+class XMLMap extends HashMap<String, Object>{
+	public String toString() {
+		String str = "";
+		for(String key : this.keySet()) {
+			str += "<" + key + ">" + "\n";
+			str += this.get(key).toString() + "\n";
+			str += "</" + key + ">" + "\n";
+		}
+		return str;
+	}
+}
